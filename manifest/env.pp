4e23bb73bdc1ab4e24b586be318ccf7e546776af
Rafael_Eduardo <rafa@genia>
2019-02-13 18:49:33 -0600

    new test

--- manifest/env.pp
+++ manifest/env.pp
@@ -11,6 +11,7 @@ package { 'links2':
 file { '/home/admin/scripts/git_updater_app.sh':
   ensure => present,
   content => "#!/bin/bash
+  killall node
   cd /home/admin/deploy
 if [[ `git status --untracked-files=no --porcelain` ]]; then
   git pull
